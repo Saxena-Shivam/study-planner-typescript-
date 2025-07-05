190d4fea14e677273acacc3e34ae898149e9c057
Shivam Saxena <shivamsaxena862006@gmail.com>
2025-07-05 15:27:34 +0530

    vercel file added

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://study-planner-typescript.onrender.com/api";
 
 export async function getStudents() {
   const res = await fetch(`${BASE_URL}/students`);
